5414b3dce255dc589841263899c8d5bbd038808a
cecamillo <cesarcamillo2212@gmail.com>
2026-04-10 23:44:58 -0300

    feat(shared): re-export xp, lives, and auth modules from index

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,6 @@ export * from "./questions";
 export * from "./subjects";
 export * from "./sessions";
 export * from "./sm2";
+export * from "./xp";
+export * from "./lives";
+export * from "./auth";
